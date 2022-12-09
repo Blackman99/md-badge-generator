a20af7dc73f6d6bf758a29de22101d74053d4d90
Dongsheng Zhao <41723543+Blackman99@users.noreply.github.com>
2022-12-09 13:17:03 +0800

    feat: support spa route change

--- md-badge-generator.user.js
+++ md-badge-generator.user.js
@@ -4,16 +4,19 @@
 // @version      0.0.1
 // @description  A user script to add markdown badges generator form to your github page by using [Shields](https://shields.io/)
 // @author       Dongsheng Zhao
-// @match        https://github.com/**/*/pull/*
-// @match        https://github.com/**/*/issues/*
-// @match        https://github.com/**/*/edit/**/*.md
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @match        https://github.com/**/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    // Your code here...
+(function () {
+  'use strict';
+  // Your code here...
+  const routeMather = /https:\/\/github\.com\/\S+\/(pull\/[0-9]+)|(edit\/\S+\.md)|(issues\/\S+)/
+
+  function addBadgeForm() {
+    if(document.querySelector('#badge-form')) {
+      return
+    }
     const badgeStyle = document.createElement('style')
     badgeStyle.textContent = `
 #badge-form {
@@ -70,25 +73,50 @@
     document.body.append(container)
 
 
-  function __copyBadgeMd () {
-    const form = document.querySelector('#badge-form')
-    const fd = new FormData(form)
-    const label = fd.get('badge-label')
-    const title = fd.get('badge-title')
-    const link = fd.get('badge-link')
-    const color = fd.get('badge-color')
-    const shieldsLink = `https://img.shields.io/badge/${encodeURIComponent(label)}-${encodeURIComponent(title)}-${encodeURIComponent(color)}`
-    navigator.clipboard.writeText(`[![](${shieldsLink})](${link})`)
-    const originText = this.innerHTML
-    this.innerHTML = `
+    function __copyBadgeMd() {
+      const form = document.querySelector('#badge-form')
+      const fd = new FormData(form)
+      const label = fd.get('badge-label')
+      const title = fd.get('badge-title')
+      const link = fd.get('badge-link')
+      const color = fd.get('badge-color')
+      const shieldsLink = `https://img.shields.io/badge/${encodeURIComponent(label)}-${encodeURIComponent(title)}-${encodeURIComponent(color)}`
+      navigator.clipboard.writeText(`[![](${shieldsLink})](${link})`)
+      const originText = this.innerHTML
+      this.innerHTML = `
  <svg class="octicon octicon-check mx-auto d-block color-fg-success" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
    <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
  </svg>
  Copied`
-    setTimeout(() => {
-      this.innerHTML = originText
-    }, 2000)
+      setTimeout(() => {
+        this.innerHTML = originText
+      }, 2000)
+    }
+
+    document.querySelector('#badge-form-copy-btn').onclick = __copyBadgeMd
   }
 
-  document.querySelector('#badge-form-copy-btn').onclick = __copyBadgeMd
+  function removeBadgeForm () {
+    const form = document.querySelector('#badge-form')
+    if(form) {
+      form.remove()
+    }
+  }
+
+
+  function reset () {
+    if (routeMather.test(location.href)) {
+      addBadgeForm()
+    } else {
+      removeBadgeForm()
+    }
+  }
+  
+  const originPushState = history.pushState
+  history.pushState = (...args) => {
+    requestAnimationFrame(reset)
+    return originPushState(...args)
+  }
+
+  reset()
 })();
