391d964f5ffdd6f4b7f60d9f54a38adfdd0008fd
Dongsheng Zhao <41723543+Blackman99@users.noreply.github.com>
2022-12-09 11:16:29 +0800

    Update md-badge-generator.user.js

--- md-badge-generator.user.js
+++ md-badge-generator.user.js
@@ -2,7 +2,7 @@
 // @name         Badge Markdown Generator
 // @namespace    https://github.com/Blackman99/md-badge-generator/raw/main/md-badge-generator.user.js
 // @version      0.0.1
-// @description  try to take over the world!
+// @description  A user script to add markdown badges generator form to your github page by using [Shields](https://shields.io/)
 // @author       Dongsheng Zhao
 // @match        https://github.com/**/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
