d8113809f2d2e2afb498eeff411fae8b610e055b
Dongsheng Zhao <41723543+Blackman99@users.noreply.github.com>
2022-12-09 11:13:32 +0800

    Update md-badge-generator.user.js

--- md-badge-generator.user.js
+++ md-badge-generator.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Badge Markdown Generator
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Blackman99/md-badge-generator
 // @version      0.0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Dongsheng Zhao
 // @match        https://github.com/**/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
